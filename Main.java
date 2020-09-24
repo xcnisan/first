fb7d2cc7ac74b52882555ab027db1fc8df0eb7c5
XCnisan <1156372639@qq.com>
2020-09-24 17:14:44 +0800

    first

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("byte的取值范围:"+Byte.MIN_VALUE+"至"+Byte.MAX_VALUE);
+	int a=20332;
+	System.out.println(a);
+	char b=20332;
+	System.out.println(b);
+	int i = 20332;
+	System.out.println((char)i);
+	char u[]={'梁','策'};
+	System.out.println(u);
+	System.out.println(""+u);
+	System.out.println((int)'王');
+	//System.out.println((int)"王");
+	System.out.println('你'+'好');
+	System.out.println("你"+"好");
+    System.out.println(""+'你'+'好');
+    char d=43229;
+    System.out.println(d);
+
+    }
+}
